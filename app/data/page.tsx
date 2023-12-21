673acb8fa2ea4a46bde6a3106f5bfeb9888e0796
Marcus Young <57845573+DMarcusYoung@users.noreply.github.com>
2023-12-20 22:19:10 -0800

    added function to get access token from the param code

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -6,6 +6,23 @@ export default function Data() {
     const searchParams = useSearchParams()
     const code = searchParams.get('code')
     console.log(code)
+    if (!code) {
+        return "No code found"
+    }
+    getAccessToken(code)
+    async function getAccessToken(code:string) {
+        const res = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify({
+                'code': code,
+                'grant_type': 'authorization_code',
+                'redirect_uri': 'http://mytestapp.com/redirect_uri',
+                'client_id': 'abcdefg12345',
+                'client_secret': 'abcdefg12345'
+            })
+        });
+        const data = await res.json();
+    }
 
   return (
      <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 min-h-screen flex flex-col items-center justify-center">
